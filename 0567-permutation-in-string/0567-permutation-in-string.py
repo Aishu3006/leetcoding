0301f6df978478103b0f163c428c9741ae5031c4
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2024-03-16 12:32:07 -0700

    Time: 88 ms (40.89%), Space: 16.8 MB (15.70%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,18 +1,19 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        len_s1, len_s2 = len(s1), len(s2)
-        if len_s1 > len_s2:
+        s1_len, s2_len = len(s1), len(s2)
+        if s1_len>s2_len:
             return False 
         
         s1_counter = Counter(s1)
-        window_counter = Counter(s2[:len(s1)])
+        window_counter = Counter(s2[:s1_len])
         
-        for r in range(len_s1,len_s2):
-            if s1_counter == window_counter:
+        for i in range(s1_len, s2_len):
+            if s1_counter==window_counter:
                 return True
             
-            window_counter[s2[r-len_s1]] -= 1
-            window_counter[s2[r]] += 1
-        
+            window_counter[s2[i-s1_len]] -= 1
+            window_counter[s2[i]] += 1
+            
         return s1_counter==window_counter
+            
         
