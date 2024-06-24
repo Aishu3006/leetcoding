fb1c9a22edf62670e936c1a8e0ad6769ba9035ce
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2024-06-24 09:45:09 -0700

    Time: 251 ms (97.15%), Space: 25.7 MB (77.65%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,12 +3,12 @@ class Solution:
         res = [1]*len(nums)
         
         #prefix
-        for i in range(1,len(nums)):
+        for i in range(1, len(res)):
             res[i] = res[i-1]*nums[i-1]
         
         #postfix
         postfix = 1
-        for i in range(len(nums)-1, -1, -1):
+        for i in range(len(res)-1, -1, -1):
             res[i] *= postfix
             postfix *= nums[i]
         
