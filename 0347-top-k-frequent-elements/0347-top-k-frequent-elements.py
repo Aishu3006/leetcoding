7d9629974fe199be42bc8e620f47177b9d144fe4
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-01-04 15:53:48 -0700

    Time: 11 ms (24.91%), Space: 24.2 MB (6.36%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,20 +1,41 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        Count = {}
+        count = {}
         freq = [[] for i in range(len(nums)+1)]
-        
+
         for n in nums:
-            Count[n] = 1 + Count.get(n,0)
+            count[n] = 1 + count.get(n, 0)
         
-        for n,c in Count.items():
-            freq[c].append(n)
+        for val, fre in count.items():
+            freq[fre].append(val)
         
         res = []
         for i in range(len(freq)-1, -1, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res)==k:
+            for val in freq[i]:
+                res.append(val)
+                if len(res) == k:
                     return res
+        
+        
+        
+        
+        
+        
+#         Count = {}
+#         freq = [[] for i in range(len(nums)+1)]
+        
+#         for n in nums:
+#             Count[n] = 1 + Count.get(n,0)
+        
+#         for n,c in Count.items():
+#             freq[c].append(n)
+        
+#         res = []
+#         for i in range(len(freq)-1, -1, -1):
+#             for n in freq[i]:
+#                 res.append(n)
+#                 if len(res)==k:
+#                     return res
             
         
         
