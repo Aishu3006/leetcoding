c3dc03ef4c48a21158d1f9e84c33167411973a37
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-01-22 20:42:53 -0700

    Time: 0 ms (100%), Space: 17.8 MB (32.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,17 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        Map = {')':'(', ']':'[', '}':'{'}
+        charMap = {')':'(', '}':'{', ']':'['}
         stack = []
-        
-        for c in s:
-            if c not in Map:
-                stack.append(c)
-                continue
-            
-            if not stack or stack[-1]!=Map[c]:
-                return False
-            
-            stack.pop()
+        for char in s:
+            if char in charMap and stack:
+                prev = stack[-1]
+                if charMap[char]==prev:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
         
         return not stack
+
+
         
