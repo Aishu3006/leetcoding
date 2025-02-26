0a9b36a92310a1624387a5dc204422d983698ad7
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-02-26 14:03:57 -0700

    Time: 8 ms (90.31%), Space: 56.9 MB (11.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    const countS = {};
+    const countT = {};
+
+    if (s.length!==t.length){
+        return false;
+    }
+
+    for(let i=0; i<s.length; i++){
+        countS[s[i]] = 1 + (countS[s[i]] || 0);
+        countT[t[i]] = 1 + (countT[t[i]] || 0);
+    }
+
+    for(const key in countS){
+        if(countS[key]!==countT[key]){
+            return false;
+        }
+    }
+
+    return true;
+};
