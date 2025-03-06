7355e35f2f0ce41c28df4050ef0f5e1cbd18c788
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-03-05 17:05:47 -0700

    Time: 129 ms (27.99%), Space: 17.8 MB (30.6%) - LeetHub

---
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
@@ -0,0 +1,21 @@
+'''
+Input - n (1<=n<=10^5)
+
+1 1
+2 5 --> 1 + 4(1)
+3 13 --> 1 + 4(1) + 4(2)
+4 1 + 4(1) + 4(2) + 4(3) = 25?
+'''
+
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        if n==0:
+            return 0
+        
+        res = 1
+
+        for i in range(1, n):
+            res += 4*i
+        
+        return res 
+        
