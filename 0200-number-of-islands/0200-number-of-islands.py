c267a78c4b596cfd9e09f80f4c9626aadd0bc732
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-03-06 17:55:44 -0700

    Time: 318 ms (11.98%), Space: 20.2 MB (69.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,27 +1,23 @@
 class Solution:
-    
-    def bfs(self, r, c, grid, ROWS, COLS): 
-
-        q = deque()
-        q.append((r,c))
-        directions = [[1,0], [-1,0], [0,1], [0,-1]] 
-        grid[r][c] = "0"
-
-        while q:
-            r,c = q.pop()
-            for dr, dc in directions:
-                nr = r + dr
-                nc = c + dc
-
-                if (nr in range(ROWS) and nc in range(COLS) and grid[nr][nc]=="1"):
-                    grid[nr][nc] = "0"
-                    q.append((nr,nc))
         
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
         
         islands = 0
+
+        def dfs(r,c):
+
+            if(r not in range(ROWS) or c not in range(COLS) or grid[r][c]=="0"):
+                return
+            
+            grid[r][c]= "0"
+            directions = [[1,0], [-1,0], [0,1], [0,-1]]
+
+            for dr, dc in directions:
+                nr = r+dr
+                nc = c+dc
+                dfs(nr,nc)
 
         ROWS = len(grid)
         COLS = len(grid[0])
@@ -30,7 +26,7 @@ class Solution:
             for c in range(COLS):
                 if grid[r][c]=="1":
                     islands += 1
-                    self.bfs(r,c,grid, ROWS, COLS)
+                    dfs(r,c)
 
         return islands
         
