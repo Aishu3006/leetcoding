3250fe50c3f62182748c778dd0d29956e207d829
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-03-14 12:12:12 -0700

    Time: 0 ms (100%), Space: 18 MB (8.6%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,26 +1,30 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        ans = []
+        charCounter = Counter(s)
+        maxCount, letter = 0, ''
 
-        pq =[(-count, char) for char,count in Counter(s).items()]
-        heapify(pq)
-
-        while pq:
-            firstCharCount, firstChar = heappop(pq)
-
-            if not ans or ans[-1]!=firstChar:
-                ans.append(firstChar)
-                if firstCharCount+1!=0:
-                    heappush(pq, (firstCharCount+1, firstChar))
-            
-            else:
-                if not pq: return ""
-                secondCharCount, secondChar = heappop(pq)
-                ans.append(secondChar)
-                if secondCharCount+1!=0:
-                    heappush(pq, (secondCharCount+1, secondChar))
-                heappush(pq, (firstCharCount, firstChar))
-
-        return ''.join(ans)
-                
+        for char, count in charCounter.items():
+            if count>maxCount:
+                maxCount = count
+                letter = char
         
+        if maxCount>(len(s)+1)//2:
+            return ""
+        
+        index = 0
+        ans = ['']*len(s)
+
+        while charCounter[letter]!=0:
+            ans[index] = letter
+            index += 2
+            charCounter[letter] -= 1
+        
+        for char, count in charCounter.items():
+            while count>0:
+                if index>=len(s):
+                    index = 1
+                ans[index] = char
+                count -= 1
+                index += 2
+        
+        return ''.join(ans)
