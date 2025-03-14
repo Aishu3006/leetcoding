ffd0dd817144f1671a60a597fb7e1ca0076e6f88
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-03-14 11:14:35 -0700

    Time: 349 ms (5.19%), Space: 25.6 MB (15.41%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,30 +3,34 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        
-        islands = 0
+
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        visited = set()
+        numOfIslands = 0
 
         def dfs(r,c):
 
-            if(r not in range(ROWS) or c not in range(COLS) or grid[r][c]=="0"):
+            if (r not in range(ROWS)
+            or c not in range(COLS)
+            or (r,c) in visited or
+            grid[r][c]=="0"):
                 return
             
-            grid[r][c]= "0"
-            directions = [[1,0], [-1,0], [0,1], [0,-1]]
+            visited.add((r,c))
+            directions = [[1,0],[-1,0],[0,1],[0,-1]]
 
             for dr, dc in directions:
                 nr = r+dr
                 nc = c+dc
                 dfs(nr,nc)
 
-        ROWS = len(grid)
-        COLS = len(grid[0])
-
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c]=="1":
-                    islands += 1
-                    dfs(r,c)
 
-        return islands
+                if grid[r][c]=="1" and (r,c) not in visited:
+                    numOfIslands += 1
+                    dfs(r,c)
         
+        return numOfIslands
+                    
