7a42c0fbbe391f27f038aba6eb95d52645e0ee0c
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-03-14 11:43:46 -0700

    Time: 251 ms (54.85%), Space: 23.5 MB (37.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,36 +1,60 @@
-class Solution:
-        
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+class UnionFind:
 
+    def __init__(self, grid):
+        self.count = 0
+        m = len(grid)
+        n = len(grid[0])
+        self.parent = []
+        self.rank = []
+
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c]=="1":
+                    self.count += 1
+                    self.parent.append(r*n+c)
+                else:
+                    self.parent.append(-1)
+                self.rank.append(0)
+
+    def find(self, i):
+        if self.parent[i]!=i:
+            self.parent[i] = self.find(self.parent[i])
+        
+        return self.parent[i]
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX!=rootY:
+            if self.rank[rootX]>self.rank[rootY]:
+                self.parent[rootY] = rootX
+            elif self.rank[rootY]>self.rank[rootX]:
+                self.parent[rootX] = rootY
+            else:
+                self.parent[rootY] = rootX
+                self.rank[rootX] += 1
+            self.count -= 1
+    
+    def getCount(self):
+        return self.count
+
+class Solution:    
+    def numIslands(self, grid: List[List[str]]) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
-        visited = set()
-        numOfIslands = 0
-
-        def dfs(r,c):
-
-            if (r not in range(ROWS)
-            or c not in range(COLS)
-            or (r,c) in visited or
-            grid[r][c]=="0"):
-                return
-            
-            visited.add((r,c))
-            directions = [[1,0],[-1,0],[0,1],[0,-1]]
-
-            for dr, dc in directions:
-                nr = r+dr
-                nc = c+dc
-                dfs(nr,nc)
-
+        uf = UnionFind(grid)
+        
         for r in range(ROWS):
             for c in range(COLS):
+                if grid[r][c]=="1":
+                    grid[r][c] = "0"
+                    if r-1>=0 and grid[r-1][c]=="1":
+                        uf.union(r*COLS+c, (r-1)*COLS+c)
+                    if r+1<ROWS and grid[r+1][c]=="1":
+                        uf.union(r*COLS+c, (r+1)*COLS+c)
+                    if c-1>=0 and grid[r][c-1]=="1":
+                        uf.union(r*COLS+c, r*COLS+c-1)
+                    if c+1<COLS and grid[r][c+1]=="1":
+                        uf.union(r*COLS+c, r*COLS+c+1)
 
-                if grid[r][c]=="1" and (r,c) not in visited:
-                    numOfIslands += 1
-                    dfs(r,c)
-        
-        return numOfIslands
-                    
+        return uf.getCount()
