f5b8460de5dda5c0fc9837f37ef07420e89b0197
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-03-14 11:03:52 -0700

    Time: 178 ms (19.69%), Space: 78.7 MB (27.26%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -14,9 +14,10 @@ class LRUCache:
         self.cache = {}
         self.left = Node(0,0)
         self.right = Node(0,0)
+
         self.left.next = self.right
         self.right.prev = self.left
-
+    
     def remove(self, node):
         prv, nxt = node.prev, node.next
         prv.next = nxt
@@ -26,25 +27,27 @@ class LRUCache:
         prv, nxt = self.right.prev, self.right
         node.prev = prv
         node.next = nxt
-        prv.next = nxt.prev = node
-
-    def get(self, key):
-        if key not in self.cache:
-            return -1
+        prv.next  = nxt.prev = node
+    
+    def get(self, key) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self.remove(node)
+            self.insert(node)
+            return self.cache[key].val
         
-        self.remove(self.cache[key])
-        self.insert(self.cache[key])
-        return self.cache[key].val
-
-    def put(self, key, val):
+        return -1
+            
+    
+    def put(self, key, val) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
-        
-        node = Node(key,val)
+        node = Node(key, val)
         self.cache[key] = node
         self.insert(node)
 
-        if len(self.cache)>self.cap:
+        if len(self.cache) > self.cap:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+        
