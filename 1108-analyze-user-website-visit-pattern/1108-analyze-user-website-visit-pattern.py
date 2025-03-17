94857539b0ab8bc775d1c0cf00deb142509596f7
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-03-16 21:52:36 -0700

    Time: 15 ms (22.11%), Space: 19.1 MB (7.65%) - LeetHub

--- 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.py
+++ 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.py
@@ -2,6 +2,27 @@ class Solution:
     def getCombinations(self, route, combinationSize):
         combinations = []
 
+        def dfs(i, current):
+            if len(current)==combinationSize:
+                combinations.append(tuple(current))
+                return
+            
+            if i>=len(route):
+                return
+            
+            #Take
+            current.append(route[i])
+            dfs(i+1, current)
+
+            #Not Take
+            current.pop()
+            dfs(i+1, current)
+
+        dfs(0, [])
+        return combinations
+        '''
+        combinations = []
+
         def dfs(ind, currentCombination):
             
             if len(currentCombination)==combinationSize:
@@ -22,8 +43,34 @@ class Solution:
         dfs(0, [])
         print(combinations)
         return combinations
+        '''
 
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+        userRouteMap = defaultdict(list)
+
+        for time, user, site in sorted(zip(timestamp, username, website), key = lambda x:(x[0], x[1])):
+            userRouteMap[user].append(site)
+        
+        patternFreqMap = defaultdict(int)
+        for user, webRoutes in userRouteMap.items():
+            patterns = set(self.getCombinations(webRoutes, 3))
+
+            for pattern in patterns:
+                patternFreqMap[pattern] += 1
+        
+        maxFreq = max(patternFreqMap.values())
+
+        res = []
+        for pattern, freq in patternFreqMap.items():
+            if freq==maxFreq:
+                res.append(pattern)
+        
+        if len(res)>1:
+            res.sort()
+        
+        return res[0]
+
+        '''
         userRouteMap = defaultdict(list)
         for time, user, site in sorted(zip(timestamp, username, website)):
             userRouteMap[user].append(site)
@@ -44,5 +91,6 @@ class Solution:
             res.sort()
         
         return res[0]
+        '''
 
         
