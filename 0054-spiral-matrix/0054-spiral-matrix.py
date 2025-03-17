e53e30bc6689b2d9c17f1ce6339a75a553d31e4b
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-03-16 20:35:32 -0700

    Time: 0 ms (100%), Space: 17.8 MB (61.72%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,32 +1,73 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        left, right = 0, len(matrix[0])
-        top, bottom = 0, len(matrix)
         res = []
-        
+        left = 0
+        top = 0
+        right = len(matrix[0])
+        bottom = len(matrix)
+
         while left<right and top<bottom:
-            #Get all elements in top row
-            for i in range(left,right):
+
+            # Top Row
+            for i in range(left, right):
                 res.append(matrix[top][i])
-            top +=1
-            
-            #Get all elements in right col
-            for i in range(top,bottom):
+            top+=1
+
+            #Right Col
+            for i in range(top, bottom):
                 res.append(matrix[i][right-1])
             right -= 1
-            
-            if not(left < right and top < bottom):
+
+            if not top<bottom or not left<right:
                 break
             
-            #Get all elements in bottom row
-            for i in range(right-1,left-1,-1):
+            #Bottom row (right to left)
+            for i in range(right-1, left-1, -1):
                 res.append(matrix[bottom-1][i])
             bottom -= 1
-            
-            #Get all elements in left col
+
+            #Left col (bottom to top)
             for i in range(bottom-1, top-1, -1):
                 res.append(matrix[i][left])
             left += 1
         
         return res
+
+        '''
+        left = 0
+        right = len(matrix[0])
+        
+        top = 0
+        bottom = len(matrix)
+        
+        res = []
+        
+        while(left<right and top<bottom):
+            
+            for i in range(left,right):
+                res.append(matrix[top][i])
+            top +=1
+            
+            for i in range(top, bottom):
+                res.append(matrix[i][right-1])
+            right-=1
+            
+            if not (left<right and top<bottom):
+                break
+            
+            for i in range(right-1, left-1, -1):
+                res.append(matrix[bottom-1][i])
+            bottom -= 1
+            
+            for i in range(bottom-1, top-1, -1):
+                res.append(matrix[i][left])
+            left += 1
+            
+        return res
+        '''
+            
+            
+            
+            
+                
         
