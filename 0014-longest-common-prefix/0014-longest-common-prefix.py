8e511eccea76945497e27f418e4909a82ede02c4
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-03-16 20:53:19 -0700

    Time: 2 ms (32.93%), Space: 17.9 MB (30.71%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -2,6 +2,22 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
+        
+        minLen = min(len(s) for s in strs)
+
+        for i in range(minLen):
+            c = strs[0][i]
+
+            for s in strs:
+                if s[i]!=c:
+                    return strs[0][:i]
+        
+        return strs[0][:minLen]
+
+
+        '''
+        if not strs:
+            return ""
 
         #The maximum a prefix can be amongst all 
         minLen = min(len(s) for s in strs)
@@ -14,3 +30,4 @@ class Solution:
                     return strs[0][:i]
         
         return strs[0][:minLen]
+        '''
