6dac13e92e86c749834a50d295b2bf85fa0e3fbe
Aishu3006 <65539932+Aishu3006@users.noreply.github.com>
2025-03-21 06:17:05 -0700

    Time: 15 ms (21.6%), Space: 19.3 MB (7.69%) - LeetHub

--- 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.py
+++ 1108-analyze-user-website-visit-pattern/1108-analyze-user-website-visit-pattern.py
@@ -20,30 +20,6 @@ class Solution:
 
         dfs(0, [])
         return combinations
-        '''
-        combinations = []
-
-        def dfs(ind, currentCombination):
-            
-            if len(currentCombination)==combinationSize:
-                combinations.append(tuple(currentCombination))
-                return
-            
-            if ind>=len(route):
-                return
-            
-            #Take
-            currentCombination.append(route[ind])
-            dfs(ind+1, currentCombination)
-
-            #Not Take
-            currentCombination.pop()
-            dfs(ind+1, currentCombination)
-        
-        dfs(0, [])
-        print(combinations)
-        return combinations
-        '''
 
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
         userRouteMap = defaultdict(list)
@@ -64,33 +40,8 @@ class Solution:
         for pattern, freq in patternFreqMap.items():
             if freq==maxFreq:
                 res.append(pattern)
-        
-        if len(res)>1:
+    
+        if len(res)>0:
             res.sort()
-        
-        return res[0]
 
-        '''
-        userRouteMap = defaultdict(list)
-        for time, user, site in sorted(zip(timestamp, username, website)):
-            userRouteMap[user].append(site)
-        
-        patternFreqMap = defaultdict(int)
-        for user, webRoute in userRouteMap.items():
-            patterns = set(self.getCombinations(webRoute, 3))
-            for pattern in patterns:
-                patternFreqMap[pattern] += 1
-        
-        maxVal = max(patternFreqMap.values())
-        res = []
-        for pattern, freq in patternFreqMap.items():
-            if patternFreqMap[pattern] == maxVal:
-                res.append(pattern)
-        
-        if len(res)>1:
-            res.sort()
-        
         return res[0]
-        '''
-
-        
